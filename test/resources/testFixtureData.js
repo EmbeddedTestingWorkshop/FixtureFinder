3c7bb4cc1ef6734273f23cc156f429a20c9f04b9
Phil Quinn <abetterway2feel@hotmail.co.uk>
2015-03-31 00:37:22 +0200

    added a test spec for filter

---
+++ test/resources/testFixtureData.js
@@ -0,0 +1,59 @@
+var testFixtureData = new function(){
+    return [
+        {
+          "country": "England",
+          "competition": "FA Cup",
+          "kickOff":{
+            "date": "2015-03-09",
+            "time": "00:00"
+          },
+          "homeTeam": "Manchester United",
+          "awayTeam": "Arsenal",
+          "score": "1 - 2"
+        },
+        {
+          "country": "France",
+          "competition": "Ligue 2",
+          "kickOff":{
+            "date": "2015-03-09",
+            "time": "00:00"
+          },
+          "homeTeam": "Arles-Avignon",
+          "awayTeam": "Auxerre",
+          "score": "2 - 4"
+        },
+        {
+          "country": "Germany",
+          "competition": "2nd Bundesliga",
+          "kickOff":{
+            "date": "2015-03-09",
+            "time": "00:00"
+          },
+          "homeTeam": "Karlsruher SC",
+          "awayTeam": "RasenBallsport Leipzig",
+          "score": "0 - 0"
+        },
+        {
+          "country": "Italy",
+          "competition": "Serie A",
+          "kickOff":{
+            "date": "2015-03-09",
+            "time": "00:00"
+          },
+          "homeTeam": "Juventus",
+          "awayTeam": "Sassuolo",
+          "score": "1 - 0"
+        },
+        {
+          "country": "Italy",
+          "competition": "Italy - Serie A",
+          "kickOff":{
+            "date": "2015-03-09",
+            "time": "00:00"
+          },
+          "homeTeam": "Lazio",
+          "awayTeam": "Fiorentina",
+          "score": "4 - 0"
+        }
+    ];
+}
---
+++ test/spec/fixture-filter-spec.js
@@ -0,0 +1,103 @@
+describe("A Fixture Filter", function() {
+  var testFixtures;
+  var filtered;
+
+  beforeEach(function() {
+    testFixtures = testFixtureData;
+  });
+
+  describe("given all countries and blank team", function() {
+    var filter;
+
+    it("returns the identity for the given array", function() {
+        filter = FixtureFinder.FixtureFilter("all", "");
+
+        filtered = filter(testFixtures);
+
+        expect(filtered).toEqual(testFixtures);
+    });
+
+    it("returns the identity for the given array", function() {
+            filter = FixtureFinder.FixtureFilter("all", "   ");
+
+            filtered = filter(testFixtures);
+
+            expect(filtered).toEqual(testFixtures);
+    });
+  });
+
+  describe("given 'Italy' as country filter and blank team", function() {
+    var filter;
+
+    it("returns the subset with only Italian fixtures", function() {
+      filter = FixtureFinder.FixtureFilter("Italy", "");
+
+      filtered = filter(testFixtures);
+
+      expect(filtered).toEqual(testFixtures.slice(3, 5));
+    });
+  });
+
+  describe("given all countries filter", function() {
+
+    describe("and team Filter is 'A'", function() {
+        var filter;
+
+        it("then the subset with only Teams with 'A' or 'a' are returned (in this case all)", function() {
+            filter = FixtureFinder.FixtureFilter("all", "A");
+
+            filtered = filter(testFixtures);
+
+            expect(filtered).toEqual(testFixtures);
+        });
+    });
+
+    describe("and team Filter is 'al'", function() {
+        var filter;
+
+        it("then the subset with only Teams with 'Al'(case insensitive) are returned", function() {
+            filter = FixtureFinder.FixtureFilter("all", "Al");
+
+            filtered = filter(testFixtures);
+
+            var expected = [ testFixtures[0], testFixtures[2]];
+            expect(filtered).toEqual(expected);
+        });
+    });
+
+    describe("and team Filter is 'arsenal'", function() {
+         var filter;
+
+        it("then the subset with only Teams with 'arsenal'(case insensitive) are returned", function() {
+            filter = FixtureFinder.FixtureFilter("all", "arsenal");
+
+            filtered = filter(testFixtures);
+
+            var expected = [ testFixtures[0]];
+            expect(filtered).toEqual(expected);
+        });
+
+        it("then the subset with only Teams with 'ARSENAL'(case insensitive) are returned", function() {
+            filter = FixtureFinder.FixtureFilter("all", "ARsEnAL");
+
+            filtered = filter(testFixtures);
+
+            var expected = [ testFixtures[0]];
+            expect(filtered).toEqual(expected);
+        });
+    });
+
+    describe("and team Filter is 'zyzz'", function() {
+        var filter;
+
+        it("then the subset with only Teams with 'Al'(case insensitive) are returned", function() {
+            filter = FixtureFinder.FixtureFilter("all", "zyzz");
+
+            filtered = filter(testFixtures);
+
+            var expected = [];
+            expect(filtered).toEqual(expected);
+        });
+    });
+  });
+});
