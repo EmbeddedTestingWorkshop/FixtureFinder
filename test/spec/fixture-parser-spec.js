d6c466d80436b8bfe68f11cc7a581bca998883f9
jeumann <jeumann@ebay.com>
2015-04-11 19:07:45 +0200

    Added test for fixture parser

---
+++ test/spec/fixture-parser-spec.js
@@ -0,0 +1,23 @@
+describe("A FixtureParser", function() {
+  var testFixtures;
+ 
+  beforeEach(function() {
+    testFixtures = testFixtureData;
+    FixtureFinder.currentLanguage = 'en';
+    FixtureParser.parseFixtures(testFixtures, "2015-03-09", FixtureFinder.FixtureFilter());    
+  });
+
+  describe("given a list of fixtures and no filter", function() {
+    it("will set the correct text for the number of fixtures", function() {
+      expect($('.fixtures .noOf').text()).toEqual("5 fixtures");
+    });
+
+    it("will set the correct text for the date", function() {
+      expect($('.fixtures .date strong').text()).toEqual("9th March 2015");
+    });
+
+    it("will add the correct number of fixtures in the table", function() {
+      expect($('.table .fixture').length).toEqual(5);
+    });
+  });
+});
