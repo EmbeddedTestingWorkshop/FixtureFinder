2b4aeae3e920c4fcecf1db7a1196bbf95e796ea9
jeumann <jeumann@ebay.com>
2015-04-11 19:34:34 +0200

    Added tests for parser in combination with filter, Removed jasmin-jquery matcher again because they don't provide good enough info in case of an error

--- test/spec/fixture-parser-spec.js
+++ test/spec/fixture-parser-spec.js
@@ -4,20 +4,46 @@ describe("A FixtureParser", function() {
   beforeEach(function() {
     testFixtures = testFixtureData;
     FixtureFinder.currentLanguage = 'en';
-    FixtureParser.parseFixtures(testFixtures, "2015-03-09", FixtureFinder.FixtureFilter());    
   });
 
   describe("given a list of fixtures and no filter", function() {
-    it("will set the correct text for the number of fixtures", function() {
-      expect($('.fixtures .noOf')).toHaveText("5 fixtures");
+    beforeEach(function() {
+      FixtureParser.parseFixtures(testFixtures, "2015-03-09", FixtureFinder.FixtureFilter());    
+    });
+
+    it("will set the number of fixtures", function() {
+      expect($('.fixtures .noOf').text()).toEqual("5 fixtures");
     });
 
     it("will set the correct date", function() {
-      expect($('.fixtures .date strong')).toHaveText("9th March 2015");
+      expect($('.fixtures .date strong').text()).toEqual("9th March 2015");
     });
 
     it("will add the correct number of fixtures in the table", function() {
-      expect($('.table .fixture')).toHaveLength(5);
+      expect($('.table .fixture').length).toEqual(5);
     });
+  });
+  
+  describe("given a list of fixtures with a filter for Arsenal", function() {
+    beforeEach(function() {
+      filter = FixtureFinder.FixtureFilter("all", "arsenal");
+      FixtureParser.parseFixtures(testFixtures, "2015-03-09", filter);    
+    });
+    
+    // seems to be necessary to avoid side effects
+    afterEach(function() {
+      filter = FixtureFinder.FixtureFilter();
+      FixtureParser.parseFixtures(testFixtures, "2015-03-09", filter);    
+    });
+
+    it("will set the correct number of filtered fixtures", function() {
+      expect($('.fixtures .noOf').text()).toEqual("1 fixtures");
+    });
+
+    it("will add the correct number of filtered fixtures in the table", function() {
+      expect($('.table .fixture').length).toEqual(1);
+    });
+
+
   });
 });
