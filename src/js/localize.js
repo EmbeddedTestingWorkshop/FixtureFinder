ac3755e627e05b4ac5514ea2d5458cf1839276f4
Phil Quinn <abetterway2feel@hotmail.co.uk>
2015-11-01 11:51:05 +0100

    removed swedish language refs

--- src/js/localize.js
+++ src/js/localize.js
@@ -24,29 +24,6 @@ var localizer = function (){
                     filter: "Filtern"
             }
             
-            case "sv":
-            case "swedish":
-                return {
-                    firstNavBtn: "Föregående vecka",
-                    secondNavBtn: "Föregående dag",
-                    thirdNavBtn: "i dag",
-                    fourthNavBtn: "Nästa Dag",
-                    fifthNavBtn: "Nästa Vecka",
-                    competition: "Konkurrens",
-                    kickOffDate: "Kick Off",
-                    home: "Hemma",
-                    score: "Betyg",
-                    away: "Borta",
-                    team: "Lag",
-                    fixtures: "fixturer",
-                    all: "Alla",
-                    countries: "Länder",
-                    england: "England",
-                    germany: "Tyskland",
-                    sweden: "Sverige",
-                    filter: "Filtera"
-                }
-            
             case "en":
             case "english":
             default:
