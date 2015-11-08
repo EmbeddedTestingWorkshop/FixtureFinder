e5af86050c7451281c49feac503475bc811c44fd
Phil Quinn <abetterway2feel@hotmail.co.uk>
2015-11-08 03:07:29 +0100

    swapped home and away team - data integrity bug

--- src/js/fixture-parser.js
+++ src/js/fixture-parser.js
@@ -27,9 +27,9 @@ var FixtureParser = function(){
         var listElement = '<tr class="fixture">';
             listElement = listElement + '<td class="competition"><div class="flag flag-'+fixture.country+'"></div>' + fixture.competition + '</td>';
             listElement = listElement + '<td class="kickOffDate" ><small>' + getLocalKickOffTime(fixture.kickOff.date, fixture.kickOff.time) + '</small></td>';
-            listElement = listElement + '<td class="home team"><strong>' + fixture.homeTeam +'</strong></td>';
+            listElement = listElement + '<td class="home team"><strong>' + fixture.awayTeam +'</strong></td>';
             listElement = listElement + '<td class="score">' + fixture.score.homeGoals + ':' + fixture.score.awayGoals + '</td>';
-            listElement = listElement + '<td class="away team"><strong>' + fixture.awayTeam + '</strong></td>';
+            listElement = listElement + '<td class="away team"><strong>' + fixture.homeTeam  + '</strong></td>';
             listElement = listElement + '</tr>';
         return listElement;
     };
